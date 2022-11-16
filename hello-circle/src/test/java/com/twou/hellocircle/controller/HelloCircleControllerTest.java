b8319ac582ee34c78149416d6465be8a8cf4fb33
Mandresy-Andri <andriamasinoromandresy@gmail.com>
2022-11-15 21:00:49 -0500

    Added a test for all controllers

--- hello-circle/src/test/java/com/twou/hellocircle/controller/HelloCircleControllerTest.java
+++ hello-circle/src/test/java/com/twou/hellocircle/controller/HelloCircleControllerTest.java
@@ -1,16 +1,17 @@
 package com.twou.hellocircle.controller;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HelloCircleController.class)
@@ -18,6 +19,17 @@ public class HelloCircleControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnHello() throws Exception {
+        // arrange and act
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+
+                // assert
+                .andExpect(content().string("Hello, Circle!"));
+    }
 
     @Test
     public void shouldReturnAnswer() throws Exception {
